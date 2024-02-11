e9c8c6633b2f9fb4c0c49c7a965deb990380394a
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-11 18:15:07 -0300

    hotfix: Table with Modal

--- src/components/Table/TableListHairCuts.tsx
+++ src/components/Table/TableListHairCuts.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { SwitchContext } from '@/context/SwitchContext';
 import { api } from '@/lib/api';
-import prisma from '@/lib/db';
 import React, { useContext, useEffect } from 'react'
 import { CiMapPin } from "react-icons/ci";
 import { HairCutProps } from '@/Interfaces/allInterfaces';
+import ModalEditClient from '../Modal/ModalEditHairCuts';
 
 const TableListHairCuts =  () => {
 
@@ -15,7 +15,6 @@ const TableListHairCuts =  () => {
         if (!checked){
             const fetchHairCuts = async () => {
                 const response = await api.get('/api/haircuts');
-                console.log(response.data)
                 setHaircuts(response.data)
             }
             fetchHairCuts()
@@ -26,7 +25,6 @@ const TableListHairCuts =  () => {
                         checked: checked
                     }
                 })
-                console.log(response.data)
                 setHaircuts(response.data)
             }
             fetchHairCuts()
@@ -39,20 +37,22 @@ const TableListHairCuts =  () => {
     }
 
   return (
-    <div className='flex flex-col w-[80%] mx-auto gap-4 rounded-lg mt-8'>
+    <div className='flex flex-col px-4 mx-auto gap-4 rounded-lg mt-8'>
         {haircuts.map((haircut) => {
             return (
-                <div key={haircut.id} className='flex py-3 rounded-md px-4 text-white font-semibold bg-[#222433] justify-between'>
-                    <div className='flex items-center gap-4'>
-                        <CiMapPin className='text-[#FBB231] text-2xl'/>
+                <ModalEditClient haircut={haircut} key={haircut.id}>
+                    <div key={haircut.id} className='flex w-full py-3 rounded-md px-4 text-white font-semibold bg-[#222433] justify-between'>
+                        <div className='flex items-center gap-4'>
+                            <CiMapPin className='text-[#FBB231] text-2xl'/>
+                            <div>
+                                <h1>{haircut.name}</h1>
+                            </div>
+                        </div>
                         <div>
-                            <h1>{haircut.name}</h1>
+                            <h1>{formatPrice(haircut.price)}</h1>
                         </div>
                     </div>
-                    <div>
-                        <h1>{formatPrice(haircut.price)}</h1>
-                    </div>
-                </div>
+                </ModalEditClient>
             )
         })}
     </div>
