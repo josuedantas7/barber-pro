c8a5991645557e16c540c34b499d5abf4d81a3c2
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-14 07:50:55 -0300

    style: resp MyProfile

--- src/app/minha_conta/page.tsx
+++ src/app/minha_conta/page.tsx
@@ -7,9 +7,9 @@ const MyProfile = () => {
     <div>
         <Header/>
         <div className='h-screen bg-[#12131B] text-white'>
-            <div className='flex flex-col gap-8 items-center w-[80%] mx-auto pt-20'>
+            <div className='flex flex-col gap-8 items-center w-[80%] max-[650px]:w-[95%] max-[650px]:pt-6 mx-auto pt-20'>
                 <h1 className='text-[#FBB231] text-2xl font-bold'>My profile</h1>
-                <div className='w-full p-7 bg-[#202130]'>
+                <div className='w-full p-7 bg-[#202130] rounded-md'>
                     <FormEditProfile/>
                 </div>
             </div>
